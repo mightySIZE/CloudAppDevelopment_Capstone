2aa3bc1c10939f095a7d19bbce53b99c2fef4d88
mightySIZE <mhasan1080@gmail.com>
2023-05-01 02:18:14 -0700

    updated files

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -6,16 +6,28 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 async function main(params) {
-      const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-      const cloudant = CloudantV1.newInstance({
-          authenticator: authenticator
-      });
-      cloudant.setServiceUrl(params.COUCH_URL);
-      try {
-        let dbList = await cloudant.getAllDbs();
-        return { "dbs": dbList.result };
-      } catch (error) {
-          return { error: error.description };
-      }
-}
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY  });
+    const cloudant = CloudantV1.newInstance({
+        authenticator: authenticator
+    });
+    cloudant.setServiceUrl("params.COUCH_URL");
 
+    try {
+        // Access the 'dealerships' database
+        const dbName = 'dealerships';
+        
+        // Fetch all documents from the 'dealerships' database
+        const allDocs = await cloudant.postAllDocs({
+            db: dbName,
+            includeDocs: true
+        });
+
+        // Extract the dealership documents
+        const dealerships = allDocs.result.rows.map(row => row.doc);
+
+        return { "dealerships": dealerships };
+    } catch (error) {
+        console.error('Error:', error);
+        return { error: error.description };
+    }
+}
